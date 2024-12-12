000e0f1050302678c4afa096ddc3d00fdf637d39
Douglas Santiago <douglasfssantiago@gmail.com>
2024-12-12 17:50:05 -0600

    main login created

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,8 @@
 
 import { select, getElement, listen } from "./utils.js";
 
-const VALID_CHARS_REGEX = /^[a-zA-Z\s]{2,}$/;
-const VALID_EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const dropdownBtn = select('.dropdown-btn');
 const dropdownContent = select('.dropdown-content');
-const modal = getElement('loginModal'); 
-const modalOpen = select('.login'); 
-const modalClose = select('.close'); 
-const sendMessage = select('.send-message');
-const name = getElement('name'); 
-const email = getElement('email'); 
-const subject = getElement('subject'); 
-const message = getElement('message');
-const output = select('.output');
 
 dropdownBtn.addEventListener('click', function() {
     dropdownContent.classList.toggle('show');
@@ -32,55 +21,26 @@ listen('click', window, (event) => {
     }
 });
 
-listen('click', modalOpen, (event) => { 
-    event.preventDefault();
-    modal.style.display = 'block'; 
-    document.body.classList.add('modal-open');
-}); 
+if (!localStorage.getItem('username') && !localStorage.getItem('password')) {
+    localStorage.setItem('username', 'johnsmith');
+    localStorage.setItem('password', '123456');
+}
 
-listen('click', modalClose, () => { 
-    modal.style.display = 'none'; 
-    document.body.classList.remove('modal-open');
-}); 
+const loginBtn = document.querySelector('.login-btn');
+const errorMessage = document.getElementById('error-message');
 
-listen('click', window, (event) => { 
-    if (event.target === modal) { 
-        modal.style.display = 'none'; 
-        document.body.classList.remove('modal-open'); 
-    }    
-});
+loginBtn.addEventListener('click', () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-listen('click', sendMessage, (event) => {
-    event.preventDefault();
-    const fields = [];
-    output.innerText = "";
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
 
-    if (!VALID_CHARS_REGEX.test(name.value)) {
-        fields.push('Name');        
-    }
-
-    if (!VALID_EMAIL_REGEX.test(email.value)) {
-        fields.push('Email');        
-    }
-
-    if (!VALID_CHARS_REGEX.test(subject.value)) {
-        fields.push('Subject');        
-    }
-
-    if (!VALID_CHARS_REGEX.test(message.value)) {
-        fields.push('Message');        
-    }    
-
-    if (fields.length > 0) {
-        output.style.color = '#ff0000';
-        output.innerText = `${fields.join(', ')} ${fields.length === 1 ? 'is' : 'are'} ${'not valid!'}`;
-
+    if (username === storedUsername && password === storedPassword) {
+        window.location.href = 'home.html';
     } else {
-        output.style.color = '#04a730';
-        name.value = '';
-        email.value = '';
-        subject.value = '';
-        message.value = '';
-        output.innerText = 'Your message was sent successfully!';
+        errorMessage.textContent = 'Incorrect username or password';
+        errorMessage.style.visibility = 'visible';
     }
 });
+
