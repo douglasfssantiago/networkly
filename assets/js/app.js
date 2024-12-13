1366be8a91b44b096007db6dc3768dc44b77ce05
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-12-13 13:25:32 -0600

    load users from api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,36 @@
 'use strict';
 
-import { select, selectById, listen } from "./utils.js";
-
-listen('load', window, () => { 
-    if (!localStorage.getItem('username') && !localStorage.getItem('password')) {
-        localStorage.setItem('username', 'johnsmith');
-        localStorage.setItem('password', '123456');
-    }
-});
+import { select, selectById, listen, create } from "./utils.js";
 
 const loginBtn = select('.login-btn');
+const usersContainer = select('.users');
 const errorMessage = selectById('error-message');
+const API_URL = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+}
+
+async function getUsers() {
+  try {
+    const response = await fetch(API_URL, options);
+
+    if (!response.ok) {
+      throw new Error(`${response.statusText} (${response.status})`);
+    }
+
+    const data = await response.json();
+    const users = data.results;
+
+    loadUsers(users);
+
+  } catch(error) {
+    console.log(error.message);
+  }
+}
 
 function handleLogin() {
     const username = selectById('username').value;
@@ -26,6 +46,58 @@ function handleLogin() {
         errorMessage.style.visibility = 'visible';
     }
 }
+
+function isAvailable() {
+   return  localStorage.length > 0 
+        && 'username' in localStorage 
+        && 'password' in localStorage
+}
+
+function loadUsers(users) {
+    //console.log(users);
+    if (users.length > 0) {
+        for (const user of users) {
+            loadUser(user);
+        }
+    }
+}
+
+function loadUser(user) {
+    if (JSON.stringify(user).length > 0) {
+        const userDiv = create('div');
+        const img = create('img');
+        const nameTitle = create('div');
+        const name = create('h3');
+        const title = create('h4');
+        const plus = create('div');
+
+        userDiv.classList.add('user', 'flex', 'flex-between');
+        img.src = user.picture.thumbnail;
+        nameTitle.classList.add('name-title', 'grid');
+        name.innerText = `${user.name.first} ${user.name.last}`;
+        title.innerText = `${user.location.city}`;
+        nameTitle.appendChild(name);
+        nameTitle.appendChild(title);
+        plus.classList.add('plus', 'center');
+        plus.innerHTML = `<i class="fa-solid fa-plus"></i>`;
+
+        userDiv.appendChild(img);
+        userDiv.appendChild(nameTitle);
+        userDiv.appendChild(plus);
+
+        usersContainer.appendChild(userDiv);
+    }
+
+}
+
+listen('load', window, () => { 
+    console.log(isAvailable());
+    if (!isAvailable()) {
+        localStorage.setItem('username', 'johnsmith');
+        localStorage.setItem('password', '123456');
+    }
+    getUsers();
+});
 
 listen('click', loginBtn, handleLogin);
 
